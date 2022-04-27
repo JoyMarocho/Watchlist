a10226c4849ca892b501a4e37d07369f737d30ad
JoyMarocho <joymarocho@gmail.com>
2022-04-27 22:17:37 +0300

    dynamic rputes

--- app/views.py
+++ app/views.py
@@ -1,1 +1,14 @@
 # Where we will create all our view functions
+
+from flask import render_template
+from app import app
+
+# Views
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
