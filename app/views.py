163ddd9509111b1536b557d6eeb5e6aeee2b4b69
JoyMarocho <joymarocho@gmail.com>
2022-05-03 07:29:16 +0300

    for loops,  making api call

--- app/views.py
+++ app/views.py
@@ -1,19 +1,20 @@
 from flask import render_template
 from app import app
-
+from .request import get_movies
 
 
 # Views
 @app.route('/')
 def index():
-        message = ' Hello World '
-
+    
         '''
             View root page function that returns the index page and its data
     '''
-
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+# Getting popular movie
+        popular_movies = get_movies('popular')
+        print(popular_movies)
+        title = 'Home - Welcome to The best Movie Review Website Online'
+        return render_template('index.html', title = title,popular = popular_movies)
 
 
 @app.route('/movie/<movie_id>')
